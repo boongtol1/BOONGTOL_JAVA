637625c16b0283aeb39979f0ab7e68770b2b139e
boongtol1 <boongtol@icloud.com>
2024-06-01 21:41:27 +0900

    boongtol

--- BOONGTOLJAVA/ee/kk/MainApp.java
+++
@@ -1,8 +0,0 @@
-package BOONGTOLJAVA.ee.kk; // 패키지 이름은 막 지어도 오류는 안난다. BUT, 관례를 지키는것이 좋다!
-public class MainApp {
-    public static void main(String[] args) {
-        // Calculator 클래스의 main 메소드를 실행
-        BOONGTOLJAVA.ee.Calculator.hello("a");
-        BOONGTOLJAVA.ee.kk.test.go("Moongyi");
-    }    
-}
