9b9a164832a73b8bfda5f0c9e83f840db4e50261
boongtol1 <boongtol@icloud.com>
2024-08-18 14:49:09 +0900

    boongtol

--- BOONGTOLJAVA/b_2_13_ScannerExample.java
+++ BOONGTOLJAVA/b_2_13_ScannerExample.java
@@ -3,7 +3,7 @@ package BOONGTOLJAVA;
 import java.util.Scanner; // 변수 선언에서 변수 이름 앞에 오는 것은 데이터 타입을 지정하며, 이 데이터 타입이 클래스일 수도 있고, 기본 데이터 타입일 수도 있습니다.(클래스,인터페이스,배열 = 참조타입, 기본 데이터 타입(8개) )
 // Scanner 타입의 변수는 Scanner 객체를 참조하는 변수입니다.
 // Scanner 클래스의 인스턴스를 생성하고, 이를 변수에 할당하여 다양한 메서드를 호출할 수 있습니다.('의' 논리)
-// System.in을 입력 소스로 사용하는 Scanner 객체를 생성합니다!--> (역으로) Scanner 의 객체는 System.in을 입력 소스로 받는다!
+// System.in을 입력 소스로 사용하는 Scanner 객체를 생성합니다!--> (역으로) Scanner 의 객체는 System.in을 입력으로 받는다!
 // System.in은 InputStream의 객체!
 // Scanner 클래스는 InputStream이라는 클래스의 객체를 처리!(하기 위한 생성자(객체가 만들어진 순간에 자동으로 호출되는 메서드)가 존재합니다.)--> 처리하는 과정은 나중에 공부한 후 다시보기!(지금은 잘 모르니까!) 
 // (이 생성자를 통해) InputStream 객체를 전달하여 Scanner 객체를 생성할 수 있습니다.
