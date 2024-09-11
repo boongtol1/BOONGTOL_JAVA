9ed9a4b9dbf0016164377330694534bb601b3962
boongtol <boongtol@icloud.com>
2024-09-11 15:49:35 +0900

    boongtol

---
+++ BOONGTOLJAVA/b_4_4_FloatCounterExample.java
@@ -0,0 +1,9 @@
+package BOONGTOLJAVA;
+
+public class b_4_4_FloatCounterExample {
+    public static void main(String[] args) {
+		for(float x=0.1f; x<=1.0f; x+=0.1f) {
+			System.out.println(x);
+		}
+	}
+}
