b9f2c83dda29b575256caddd985699cd4791e826
SeungHoonLee <167671024+boongtol1@users.noreply.github.com>
2025-05-11 11:18:13 +0900

    boongtol

--- src/BOONGTOL_MBP/app/src/main/java/boongtol/BOONGTOL_MBP/gik_2_3.java
+++ src/BOONGTOL_MBP/app/src/main/java/boongtol/BOONGTOL_MBP/gik_2_3.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class gik_2_3 extends AppCompatActivity {
